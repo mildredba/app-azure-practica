8496c591a2ee6af39c8fba14023ab1e3b1259bf6
mildredba <mildredbautis@gmail.com>
2026-06-30 19:26:49 -0500

    Update title in Streamlit app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # Título principal
-st.title("🚀 Mi Primera App Streamlit en Docker Mili")
+st.title("🚀 Mi Primera App Streamlit en Docker Mili <3")
 st.markdown("---")
 
 # Sidebar
