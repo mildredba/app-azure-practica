521bded76973584efcf6f9a446d84f739c722aae
Mildred <mildredbautis@gmail.com>
2026-06-30 19:22:49 -0500

    mod

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # Título principal
-st.title("🚀 Mi Primera App Streamlit en Docker")
+st.title("🚀 Mi Primera App Streamlit en Docker Mili")
 st.markdown("---")
 
 # Sidebar
